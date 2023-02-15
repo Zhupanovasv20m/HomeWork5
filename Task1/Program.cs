845f84255ddbe4d5c3343c196b7dd63cfb32fe76
alice <zhupanova2017sv@mail.ru>
2023-02-15 20:55:02 +0300

    Find Count of even numbers

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] arr = GetRandomArray(4);
+PrintArray(arr);
+System.Console.WriteLine(FindCountOfEvenNum(arr));
+
+int FindCountOfEvenNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+    System.Console.WriteLine();
+}
